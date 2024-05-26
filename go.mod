4260893f21700716765e98c5865c7d4f47b5ca5b
ab-dauletkhan <ab.dauletkhan@gmail.com>
2024-05-26 15:41:18 +0500

    practiced with pointers

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module first_time
 
 go 1.22.3
-
-require github.com/google/uuid v1.6.0
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+// func main() {
+// 	// arrForSlice := [5]int{1, 2, 3, 4, 5}
+// 	// sliceFromArr := arrForSlice[1:4]
+// 	// fmt.Println("Slice from array:", sliceFromArr)
+// 	// arrForSlice[3] = 9
+// 	// fmt.Println(arrForSlice)
+// 	// fmt.Println(sliceFromArr)
+// 	// sliceFromArr[2] = 1
+// 	// fmt.Println(arrForSlice)
+// 	// fmt.Println(sliceFromArr)
+
+// 	// fmt.Printf("%d - %d - %d\n", 5, 4)
+// }
